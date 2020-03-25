8f0a0b504cb087424b1544134a9d68ed555c7808
Otto Holmström <ottome@live.com>
2020-03-25 17:24:13 +0100

    working DFS

--- 2wordladders/sol/solution.cpp
+++ 2wordladders/sol/solution.cpp
@@ -8,43 +8,52 @@
 
 bool sameLastLetters(std::string *s1, std::string *s2)
 {
-  if (s1 == s2)
+  if (s1 == s2 || s1->compare(*s2) == 0)
   { //Same reference
     return false;
   }
 
   const char *charSeq1 = s1->c_str();
   const char *charSeq2 = s2->c_str();
-  std::vector<char> s2CharVec;
+  int s1int[5];
+  int s2int[5];
 
-  for (int i = 1; i < 6; ++i)
+  for (int i = 0; i < 5; i++)
   {
-    s2CharVec.push_back(charSeq2[i]);
+    s1int[i] = charSeq1[i];
+    s2int[i] = charSeq2[i];
   }
 
   for (int i = 0; i < 5; i++)
   {
-    bool changed = false;
-    for (int j = 5 - i; j >= 0; --j)
+    for (int j = 1; j < 5; j++)
     {
-      std::cout << "Comparing " << s2CharVec[j] << " and " << charSeq1[i] << std::endl;
-      if (s2CharVec[j] == charSeq1[i])
+      if (s1int[j] == s2int[i])
       {
-        changed = true;
-        s2CharVec[j] = '0';
+        s1int[j] = 0;
+        s2int[i] = 1;
       }
     }
-    if (!changed)
+  }
+  for (int i = 1; i < 5; i++)
+  {
+    if (s1int[i] != 0)
     {
       return false;
     }
   }
+
   return true;
 }
 
 //Breadth-first-search on the graph graph, with start start and end end. Returns path length, or -1 if no path was found.
 int BFS(std::map<std::string, std::list<std::string> *> *graph, std::string *start, std::string *end)
 {
+  if (start->compare(*end) == 0)
+  {
+    return 0;
+  }
+
   std::map<std::string, int> visitedMap;
   std::map<std::string, std::string> prevMap;
 
@@ -105,7 +114,7 @@ int main()
       std::getline(std::cin, line);
       if (!line.empty())
       {
-        std::cout << line << std::endl;
+        //std::cout << line << std::endl;
         inputWords.push_back(line);
         graph[line] = new std::list<std::string>;
       }
@@ -138,11 +147,11 @@ int main()
     std::string *string1 = new std::string(part1);
     std::string *string2 = new std::string(part2);
 
-    std::cout << *string1 << " " << *string2 << "\n";
+    //std::cout << *string1 << " " << *string2 << "\n";
     pathToTest[string1] = string2;
   }
 
-  for (std::map<std::string, std::list<std::string> *>::iterator it = graph.begin(); it != graph.end(); ++it)
+  /* for (std::map<std::string, std::list<std::string> *>::iterator it = graph.begin(); it != graph.end(); ++it)
   {
     std::cout << it->first << " has neighbours ";
     std::list<std::string> *list = it->second;
@@ -151,9 +160,9 @@ int main()
       std::cout << *it2 << ",";
     }
     std::cout << std::endl;
-  }
+  } */
 
-  /* for (std::map<std::string *, std::string *>::iterator it = pathToTest.begin(); it != pathToTest.end(); ++it)
+  for (std::map<std::string *, std::string *>::iterator it = pathToTest.begin(); it != pathToTest.end(); ++it)
   {
     int res = BFS(&graph, it->first, it->second);
     if (res >= 0)
@@ -165,5 +174,5 @@ int main()
       std::cout << "Impossible";
     }
     std::cout << std::endl;
-  } */
+  }
 }
