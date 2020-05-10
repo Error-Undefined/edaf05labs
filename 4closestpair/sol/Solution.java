3de6131a208eb782d85d84b5a57d576d0c8d155a
Otto Holmström <ottome@live.com>
2020-05-10 17:12:23 +0200

    progress?

--- 4closestpair/sol/Solution.java
+++ 4closestpair/sol/Solution.java
@@ -1,9 +1,81 @@
 package sol;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
+  double distance(Point p1, Point p2) {
+    return Math.hypot(p2.x - p1.x, p2.y - p1.y);
+  }
+
+  double bruteForce(Point[] xArray, int points) {
+    if (points == 2) {
+      return distance(xArray[0], xArray[1]);
+    }
+
+    double minDistance = Double.MAX_VALUE;
+    for (int i = 0; i < points; i++) {
+      for (int j = i + 1; j < points; j++) {
+        double tempDistance = distance(xArray[i], xArray[j]);
+        minDistance = Math.min(tempDistance, minDistance);
+      }
+    }
+    return minDistance;
+  }
+
+  double closestRecursive(Point[] xArray, Point[] yArray, int xStart, int xEnd) {
+    int points = xEnd - xStart;
+
+    if (points < 4) {
+      return bruteForce(xArray, points);
+    }
+
+    int midIndex = xStart + (points + 1) / 2;
+    Point midPoint = xArray[midIndex];
+
+    Point[] yLeft = new Point[midIndex - xStart];
+    Point[] yRight = new Point[xEnd - midIndex];
+
+    int leftIndex = 0;
+    int rightIndex = 0;
+    for (int i = 0; i < yArray.length; i++) {
+      if (yArray[i].x <= midPoint.x && leftIndex < yLeft.length) {
+        yLeft[leftIndex++] = yArray[i];
+      } else if (rightIndex < yRight.length) {
+        yRight[rightIndex++] = yArray[i];
+      } else {
+        yLeft[leftIndex++] = yArray[i];
+      }
+    }
+
+    System.out.println(leftIndex + ", " + yLeft.length);
+    System.out.println(rightIndex + ", " + yRight.length);
+    System.out.println(points);
+
+    double dL = closestRecursive(xArray, yLeft, xStart, midIndex);
+    double dR = closestRecursive(xArray, yRight, midIndex, xEnd);
+
+    double delta = Math.min(dL, dR);
+
+    List<Point> lineSet = new ArrayList<>();
+    for (Point p : xArray) {
+      if (Math.abs(p.x - midPoint.x) < delta) {
+        lineSet.add(p);
+      }
+    }
+
+    double minInSet = Double.MAX_VALUE;
+    for (int i = 0; i < lineSet.size(); i++) {
+      for (int j = i + 1; j < lineSet.size() && j < i + 15; j++) {
+        double tempDistance = distance(lineSet.get(i), lineSet.get(j));
+        minInSet = Math.min(tempDistance, minInSet);
+      }
+    }
+
+    return Math.min(minInSet, delta);
+  }
 
   void run() {
     Scanner inputScanner = new Scanner(System.in);
@@ -25,6 +97,9 @@ public class Solution {
     Arrays.sort(xArray, (p1, p2) -> p1.x - p2.x);
     Arrays.sort(yArray, (p1, p2) -> p1.y - p2.y);
 
+    double result = closestRecursive(xArray, yArray, 0, points);
+
+    System.out.println(result);
   }
 
   public static void main(String[] args) {
--- 4closestpair/sol/solution.cpp
+++ 4closestpair/sol/solution.cpp
@@ -18,6 +18,55 @@ double min(double d1, double d2)
   return (d1 < d2) ? d1 : d2;
 }
 
+double bruteForce(point **yArray, int points)
+{
+  double distance01 = dist(yArray[0], yArray[1]);
+
+  if (points == 2)
+  {
+    return distance01;
+  }
+
+  double distance12 = dist(yArray[1], yArray[2]);
+  double distance20 = dist(yArray[2], yArray[0]);
+
+  return min(min(distance01, distance12), distance20);
+}
+
+double closestRecursive(point **xArray, point **yArray, int points)
+{
+  if (points < 4)
+  {
+    return bruteForce(yArray, points);
+  }
+
+  int midIndex = (points + 1) / 2;
+  point *midPoint = xArray[midIndex];
+
+  point *yLeft[midIndex];
+  point *yRight[points - midIndex];
+
+  int leftIndex = 0;
+  int rightIndex = 0;
+
+  for (int i = 0; i < points; i++)
+  {
+    if (yArray[i]->x <= midPoint->x && leftIndex < midIndex)
+    {
+      yLeft[leftIndex++] = yArray[i];
+    }
+    else
+    {
+      yRight[rightIndex++] = yArray[i];
+    }
+  }
+
+  double dL = closestRecursive(xArray, yLeft, midIndex);
+  double dR = closestRecursive(xArray + midIndex, yRight, points - midIndex);
+
+  double delta = min(dL, dR);
+}
+
 int main()
 {
   int points;
